e0ea0ca3d548d096d33dfa32b1f3b0814be3a452
lutfaainulhasanah <lutfa.hasanah24@gmail.com>
2018-05-09 19:05:32 +0700

    .htaccess commit

---
+++ application/views/Home_view.php
@@ -0,0 +1,384 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+<!-- BEGIN HEAD -->
+<head>
+    <meta charset="UTF-8" />
+    <title>BCORE Admin Dashboard Template | Dashboard </title>
+     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+	<meta content="" name="description" />
+	<meta content="" name="author" />
+     <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <!-- GLOBAL STYLES -->
+    <link rel="stylesheet" href="<?php echo base_url().'assets/assets/plugins/bootstrap/css/bootstrap.css'?>" />
+    <link rel="stylesheet" href="<?php echo base_url().'assets/assets/css/main.css'?>" />
+    <link rel="stylesheet" href="<?php echo base_url().'assets/assets/css/theme.css'?>" />
+    <link rel="stylesheet" href="<?php echo base_url().'assets/assets/css/MoneAdmin.css'?>" />
+    <link rel="stylesheet" href="<?php echo base_url().'assets/assets/plugins/Font-Awesome/css/font-awesome.css'?>" />
+    <!--END GLOBAL STYLES -->
+
+    <!-- PAGE LEVEL STYLES -->
+    <link href="<?php echo base_url().'assets/assets/css/layout2.css'?>" rel="stylesheet" />
+       <link href="<?php echo base_url().'assets/assets/plugins/flot/examples/examples.css'?>" rel="stylesheet" />
+       <link rel="stylesheet" href="<?php echo base_url().'assets/assets/plugins/timeline/timeline.css'?>" />
+    <!-- END PAGE LEVEL  STYLES -->
+     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+    <!-- END HEAD -->
+
+    <!-- BEGIN BODY -->
+<body class="padTop53 " >
+
+    <!-- MAIN WRAPPER -->
+    <div id="wrap" >
+        
+
+        <!-- HEADER SECTION -->
+        <div id="top">
+
+            <nav class="navbar navbar-inverse navbar-fixed-top " style="padding-top: 10px;">
+                <a data-original-title="Show/Hide Menu" data-placement="bottom" data-tooltip="tooltip" class="accordion-toggle btn btn-primary btn-sm visible-xs" data-toggle="collapse" href="#menu" id="menu-toggle">
+                    <i class="icon-align-justify"></i>
+                </a>
+                <!-- LOGO SECTION -->
+                <header class="navbar-header">
+
+                    <a href="index.html" class="navbar-brand">
+                    <img src="<?php echo base_url().'assets/assets/img/logo.png'?>" alt="" />
+                        
+                        </a>
+                </header>
+                <!-- END LOGO SECTION -->
+                <ul class="nav navbar-top-links navbar-right">
+
+                    <!--ADMIN SETTINGS SECTIONS -->
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="icon-user "></i>&nbsp; <i class="icon-chevron-down "></i>
+                        </a>
+
+                        <ul class="dropdown-menu dropdown-user">
+                            <li><a href="#"><i class="icon-user"></i> User Profile </a>
+                            </li>
+                            <li><a href="#"><i class="icon-gear"></i> Settings </a>
+                            </li>
+                            <li class="divider"></li>
+                            <li><a href="login.html"><i class="icon-signout"></i> Logout </a>
+                            </li>
+                        </ul>
+
+                    </li>
+                    <!--END ADMIN SETTINGS -->
+                </ul>
+
+            </nav>
+
+        </div>
+        <!-- END HEADER SECTION -->
+
+
+
+        <!-- MENU SECTION -->
+       <div id="left" >
+            <ul id="menu" class="collapse">
+                <li class="panel active">
+                    <a href="index.html" >
+                        <i class="icon-table"></i> Dashboard
+                    </a>                   
+                </li>
+
+                <li class="panel ">
+                    <a href="#" data-parent="#menu" data-toggle="collapse" class="accordion-toggle" data-target="#component-nav">
+                        <i class="icon-tasks"> </i> UI Elements     
+	   
+                        <span class="pull-right">
+                          <i class="icon-angle-left"></i>
+                        </span>
+                       &nbsp; <span class="label label-default">10</span>&nbsp;
+                    </a>
+                    <ul class="collapse" id="component-nav">
+                       
+                        <li class=""><a href="button.html"><i class="icon-angle-right"></i> Buttons </a></li>
+                         <li class=""><a href="icon.html"><i class="icon-angle-right"></i> Icons </a></li>
+                        <li class=""><a href="progress.html"><i class="icon-angle-right"></i> Progress </a></li>
+                        <li class=""><a href="tabs_panels.html"><i class="icon-angle-right"></i> Tabs & Panels </a></li>
+                        <li class=""><a href="notifications.html"><i class="icon-angle-right"></i> Notification </a></li>
+                         <li class=""><a href="more_notifications.html"><i class="icon-angle-right"></i> More Notification </a></li>
+                        <li class=""><a href="modals.html"><i class="icon-angle-right"></i> Modals </a></li>
+                          <li class=""><a href="wizard.html"><i class="icon-angle-right"></i> Wizard </a></li>
+                         <li class=""><a href="sliders.html"><i class="icon-angle-right"></i> Sliders </a></li>
+                        <li class=""><a href="typography.html"><i class="icon-angle-right"></i> Typography </a></li>
+                    </ul>
+                </li>
+                <li class="panel ">
+                    <a href="#" data-parent="#menu" data-toggle="collapse" class="accordion-toggle collapsed" data-target="#form-nav">
+                        <i class="icon-pencil"></i> Forms
+	   
+                        <span class="pull-right">
+                            <i class="icon-angle-left"></i>
+                        </span>
+                          &nbsp; <span class="label label-success">5</span>&nbsp;
+                    </a>
+                    <ul class="collapse" id="form-nav">
+                        <li class=""><a href="forms_general.html"><i class="icon-angle-right"></i> General </a></li>
+                        <li class=""><a href="forms_advance.html"><i class="icon-angle-right"></i> Advance </a></li>
+                        <li class=""><a href="forms_validation.html"><i class="icon-angle-right"></i> Validation </a></li>
+                        <li class=""><a href="forms_fileupload.html"><i class="icon-angle-right"></i> FileUpload </a></li>
+                        <li class=""><a href="forms_editors.html"><i class="icon-angle-right"></i> WYSIWYG / Editor </a></li>
+                    </ul>
+                </li>
+
+                <li class="panel">
+                    <a href="#" data-parent="#menu" data-toggle="collapse" class="accordion-toggle" data-target="#pagesr-nav">
+                        <i class="icon-table"></i> Pages
+	   
+                        <span class="pull-right">
+                            <i class="icon-angle-left"></i>
+                        </span>
+                          &nbsp; <span class="label label-info">6</span>&nbsp;
+                    </a>
+                    <ul class="collapse" id="pagesr-nav">
+                        <li><a href="pages_calendar.html"><i class="icon-angle-right"></i> Calendar </a></li>
+                        <li><a href="pages_timeline.html"><i class="icon-angle-right"></i> Timeline </a></li>
+                        <li><a href="pages_social.html"><i class="icon-angle-right"></i> Social </a></li>
+                        <li><a href="pages_pricing.html"><i class="icon-angle-right"></i> Pricing </a></li>
+                        <li><a href="pages_offline.html"><i class="icon-angle-right"></i> Offline </a></li>
+                        <li><a href="pages_uc.html"><i class="icon-angle-right"></i> Under Construction </a></li>
+                    </ul>
+                </li>
+                <li class="panel">
+                    <a href="#" data-parent="#menu" data-toggle="collapse" class="accordion-toggle" data-target="#chart-nav">
+                        <i class="icon-bar-chart"></i> Charts
+	   
+                        <span class="pull-right">
+                            <i class="icon-angle-left"></i>
+                        </span>
+                          &nbsp; <span class="label label-danger">4</span>&nbsp;
+                    </a>
+                    <ul class="collapse" id="chart-nav">
+
+
+
+                        <li><a href="charts_line.html"><i class="icon-angle-right"></i> Line Charts </a></li>
+                        <li><a href="charts_bar.html"><i class="icon-angle-right"></i> Bar Charts</a></li>
+                        <li><a href="charts_pie.html"><i class="icon-angle-right"></i> Pie Charts </a></li>
+                        <li><a href="charts_other.html"><i class="icon-angle-right"></i> other Charts </a></li>
+                    </ul>
+                </li>
+
+                <li class="panel">
+                    <a href="#" data-parent="#menu" data-toggle="collapse" class="accordion-toggle" data-target="#DDL-nav">
+                        <i class=" icon-sitemap"></i> 3 Level Menu
+	   
+                        <span class="pull-right">
+                            <i class="icon-angle-left"></i>
+                        </span>
+                    </a>
+                    <ul class="collapse" id="DDL-nav">
+                        <li>
+                            <a href="#" data-parent="#DDL-nav" data-toggle="collapse" class="accordion-toggle" data-target="#DDL1-nav">
+                                <i class="icon-sitemap"></i>&nbsp; Demo Link 1
+	   
+                        <span class="pull-right" style="margin-right: 20px;">
+                            <i class="icon-angle-left"></i>
+                        </span>
+
+
+                            </a>
+                            <ul class="collapse" id="DDL1-nav">
+                                <li>
+                                    <a href="#"><i class="icon-angle-right"></i> Demo Link 1 </a>
+
+                                </li>
+                                <li>
+                                    <a href="#"><i class="icon-angle-right"></i> Demo Link 2 </a></li>
+                                <li>
+                                    <a href="#"><i class="icon-angle-right"></i> Demo Link 3 </a></li>
+
+                            </ul>
+
+                        </li>
+                        <li><a href="#"><i class="icon-angle-right"></i> Demo Link 2 </a></li>
+                        <li><a href="#"><i class="icon-angle-right"></i> Demo Link 3 </a></li>
+                        <li><a href="#"><i class="icon-angle-right"></i> Demo Link 4 </a></li>
+                    </ul>
+                </li>
+                <li class="panel">
+                    <a href="#" data-parent="#menu" data-toggle="collapse" class="accordion-toggle" data-target="#DDL4-nav">
+                        <i class=" icon-folder-open-alt"></i> 4 Level Menu
+	   
+                        <span class="pull-right">
+                            <i class="icon-angle-left"></i>
+                        </span>
+                    </a>
+                    <ul class="collapse" id="DDL4-nav">
+                        <li>
+                            <a href="#" data-parent="DDL4-nav" data-toggle="collapse" class="accordion-toggle" data-target="#DDL4_1-nav">
+                                <i class="icon-sitemap"></i>&nbsp; Demo Link 1
+	   
+                        <span class="pull-right" style="margin-right: 20px;">
+                            <i class="icon-angle-left"></i>
+                        </span>
+
+
+                            </a>
+                            <ul class="collapse" id="DDL4_1-nav">
+                                <li>
+
+                                    <a href="#" data-parent="#DDL4_1-nav" data-toggle="collapse" class="accordion-toggle" data-target="#DDL4_2-nav">
+                                        <i class="icon-sitemap"></i>&nbsp; Demo Link 1
+	   
+                        <span class="pull-right" style="margin-right: 20px;">
+                            <i class="icon-angle-left"></i>
+                        </span>
+                                    </a>
+                                    <ul class="collapse" id="DDL4_2-nav">
+
+
+
+                                        <li><a href="#"><i class="icon-angle-right"></i> Demo Link 1 </a></li>
+                                        <li><a href="#"><i class="icon-angle-right"></i> Demo Link 2 </a></li>
+                                    </ul>
+
+
+
+                                </li>
+                                <li><a href="#"><i class="icon-angle-right"></i> Demo Link 2 </a></li>
+                                <li><a href="#"><i class="icon-angle-right"></i> Demo Link 3 </a></li>
+                            </ul>
+
+                        </li>
+                        <li><a href="#"><i class="icon-angle-right"></i> Demo Link 2 </a></li>
+                        <li><a href="#"><i class="icon-angle-right"></i> Demo Link 3 </a></li>
+                        <li><a href="#"><i class="icon-angle-right"></i> Demo Link 4 </a></li>
+                    </ul>
+                </li>
+
+                <li><a href="gallery.html"><i class="icon-film"></i> Image Gallery </a></li>
+                <li><a href="tables.html"><i class="icon-table"></i> Data Tables </a></li>
+                <li><a href="maps.html"><i class="icon-map-marker"></i> Maps </a></li>
+
+            </ul>
+
+        </div>
+        <!--END MENU SECTION -->
+
+
+
+        <!--PAGE CONTENT -->
+        <div id="content">
+             
+            <div class="inner" style="min-height: 700px;">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1> Admin Dashboard </h1>
+                    </div>
+                </div>
+                  <hr />
+                   <!-- CHART & CHAT  SECTION -->                                  
+			<div class="demo-container">
+			<div id="placeholderRT" class="demo-placeholder"></div>
+		</div>
+                 <!--END CHAT & CHAT SECTION -->             
+
+                 <!--  STACKING CHART  SECTION   -->
+                <div class="row">
+                   <div class="col-lg-12">
+                    <div class="panel panel-default">
+                            <div class="panel-heading">
+                            Sales   Stacking
+                            </div>
+
+                            <div class="panel-body">
+                              
+			<div class="demo-container">
+			<div id="placeholderStack" class="demo-placeholder"></div>
+		</div>
+                                <p class="stackControls">
+			<button class="btn btn-primary" >With stacking</button>
+			<button class="btn btn-primary">Without stacking</button>
+		</p>
+
+		<p class="graphControls">
+			<button class="btn btn-primary">Bars</button>
+			<button class="btn btn-primary">Lines</button>
+			<button class="btn btn-primary">Lines with steps</button>
+		</p>
+		</div>
+		
+                            </div>
+                    </div>
+                     
+                </div>
+                 <!--END STACKING CHART SCETION  -->                
+            </div>
+
+        </div>
+        <!--END PAGE CONTENT -->
+
+         <!-- RIGHT STRIP  SECTION -->
+        <div id="right">
+            <div class="well well-small">
+                <span>Profit</span><span class="pull-right"><small>20%</small></span>
+
+                <div class="progress mini">
+                    <div class="progress-bar progress-bar-info" style="width: 20%"></div>
+                </div>
+                <span>Sales</span><span class="pull-right"><small>40%</small></span>
+
+                <div class="progress mini">
+                    <div class="progress-bar progress-bar-success" style="width: 40%"></div>
+                </div>
+                <span>Pending</span><span class="pull-right"><small>60%</small></span>
+
+                <div class="progress mini">
+                    <div class="progress-bar progress-bar-warning" style="width: 60%"></div>
+                </div>
+                <span>Summary</span><span class="pull-right"><small>80%</small></span>
+
+                <div class="progress mini">
+                    <div class="progress-bar progress-bar-danger" style="width: 80%"></div>
+                </div>
+            </div>
+        </div>
+         <!-- END RIGHT STRIP  SECTION -->
+    </div>
+
+    <!--END MAIN WRAPPER -->
+
+    <!-- FOOTER -->
+    <div id="footer">
+        <p>&copy;  binarytheme &nbsp;2014 &nbsp;</p>
+    </div>
+    <!--END FOOTER -->
+
+
+    <!-- GLOBAL SCRIPTS -->
+    <script src="<?php echo base_url().'assets/assets/plugins/jquery-2.0.3.min.js'?>"></script>
+     <script src="<?php echo base_url().'assets/assets/plugins/bootstrap/js/bootstrap.min.js'?>"></script>
+    <script src="<?php echo base_url().'assets/assets/plugins/modernizr-2.6.2-respond-1.1.0.min.js'?>"></script>
+    <!-- END GLOBAL SCRIPTS -->
+
+    <!-- PAGE LEVEL SCRIPTS -->
+    <script src="<?php echo base_url().'assets/assets/plugins/flot/jquery.flot.js'?>"></script>
+    <script src="<?php echo base_url().'assets/assets/plugins/flot/jquery.flot.resize.js'?>"></script>
+    <script src="<?php echo base_url().'assets/assets/plugins/flot/jquery.flot.time.js'?>"></script>
+     <script  src="<?php echo base_url().'assets/assets/plugins/flot/jquery.flot.stack.js'?>"></script>
+    <script src="<?php echo base_url().'assets/assets/js/for_index.js'?>"></script>
+   
+    <!-- END PAGE LEVEL SCRIPTS -->
+
+
+</body>
+
+    <!-- END BODY -->
+</html>
