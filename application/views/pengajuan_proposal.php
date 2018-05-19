373ca0fad5685c306d02bed8a420dfc10c315c8a
Indah Nur Rizqi Rahmadani <rizqiqiqi02@gmail.com>
2018-05-19 11:01:17 +0700

    indah commit
    pengajuan judul

---
+++ application/views/pengajuan_proposal.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pengajuan Proposal</title>
+</head>
+<body>
+	<header>
+        <center>
+            <h1>Pengajuan Proposal</h1>
+		<p>
+			<label for="cover">Cover : </label>
+			<input type="file" name="cover" placeholder="cover" />
+		</p>
+		<p>
+			<label for="bab1">BAB 1 : </label>
+			<input type="file" name="bab1" placeholder="bab1" />
+		</p>
+		<p>
+			<label for="bab2">BAB 2 : </label>
+			<input type="file" name="bab2" placeholder="bab2" />
+		</p>
+		<p>
+			<label for="bab3">BAB 3 : </label>
+			<input type="file" name="bab3" placeholder="bab3" />
+		</p>
+		<p>
+			<label for="persetujuan">Persetujuan : </label>
+			<input type="file" name="persetujuan" placeholder="perseyujuan" />
+		</p>
+
+
+<input type="submit" name="proses" value="Submit"/>
+</center>
+	</header>
+</body>
+</html>
